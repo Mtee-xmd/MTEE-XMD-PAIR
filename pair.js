5d5f4be3873212aaf15136edfe43139dea20bebb
Humbah2005 <tanakapunungwe21@gmail.com>
2025-06-14 07:29:31 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,30 +75,30 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "KANGO~" + string_session;
+                        let md = "MTEE-XMD ~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hello there KANGO-XMD User! 👋🏻* 
+                        let desc = `*Hello there MTEE-XMD User! 👋🏻* 
 
 > Do not share your session id with your gf 😂.
 
- *Thanks for using KANGO-XMD 🚩* 
+ *Thanks for using MTEE-XMD 🚩* 
 
 > Join WhatsApp Channel :- ⤵️
  
-https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E
+https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P
 
 Dont forget to fork the repo ⬇️
 
-https://github.com/OfficialKango/KANGO-XMD
+https://github.com/Mtee-xmd/MTEE-XMD 
 
-> *© Powered BY Hector Manuel 🖤*`; 
+> *© Powered BY HUMBAH 🖤*`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "Hector Manuel",
-thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
+title: "HUMBAH",
+thumbnailUrl: "https://files.catbox.moe/9b9kao.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -107,14 +107,14 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `*Don't Share with anyone this code use for deploy KANGO-XMD*\n\n ◦ *Github:* https://github.com/OfficialKango/KANGO-XMD`;
+                            let desc = `*Don't Share with anyone this code use for deploy MTEE-XMD*\n\n ◦ *Github:* https://github.com/Mtee-xmd/MTEE-XMD `;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "KANGO-XMD",
-thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
+title: "Mtee-xmd",
+thumbnailUrl: "https://files.catbox.moe/y1mwzc.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vb6EJfCHLHQQGd2KGL1P",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
